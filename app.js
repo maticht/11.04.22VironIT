9d7fe9dc15f519074a00c292379249ed3d150136
maticht <74956969+maticht@users.noreply.github.com>
2022-04-11 19:44:18 +0300

    11.04.22VironIT
    11.04.22VironIT

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express')
+let app = express()
+// const routs = require('./routes/rout')
+// app.use('/api', routs)
+module.exports = app
+
+let body
+let userArr = [
+    {
+        "id": 1,
+        "name": 'Alex',
+        "age": 50
+    },
+    {
+        "id": 2,
+        "name": 'Max',
+        "age": 40
+    },
+    {
+        "id": 3,
+        "name": 'Egor',
+        "age": 45
+    },
+    {
+        "id": 4,
+        "name": 'misha',
+        "age": 20
+    }
+]
+
+app.get('/', (req,res) => {
+    res.end(JSON.stringify(userArr.flat()))
+})
+app.get('/:id', (req,res) => {
+    res.end(JSON.stringify(userArr.flat()[req.params.id - 1]))
+})
+
+app.delete('/:id', (req,res) => {
+    delete userArr[req.params.id - 1]
+        res.end(`User with id ${req.params.id} has been deleted`)
+})
+
+app.post('/', (req,res) => {
+    body = '';
+    req.on('data', chunk => {
+        body += chunk.toString()
+    });
+    req.on('end', () => {
+        userArr.push(JSON.parse(body))
+        res.end('User has been added')
+    });
+})
+
+app.put('/', (req,res) => {
+    body = ''
+    req.on('data', chunk => {
+        body += chunk.toString()
+    });
+    req.on('end', () => {
+        userArr.splice(0,userArr.length,JSON.parse(body))
+        res.end('Array with users has been overwritten')
+    });
+})
+app.put('/:id', (req,res) => {
+    body = ''
+    req.on('data', chunk => {
+        body += chunk.toString()
+    });
+    req.on('end', () => {
+        userArr.splice(req.params.id - 1,1,JSON.parse(body))
+        res.end('Users has been overwritten')
+    });
+})
+    // [{"id":5,"name":"Dima","age":50},{"id":6,"name":"Valera","age":30},{"id":7,"name":"Pit","age":40}]
---
+++ routes/rout.js
@@ -0,0 +1,8 @@
+// const express = require('express')
+// const router = express.Router()
+//
+// router.get('/id',(req,res) => {
+//     res.end(JSON.stringify(userArr.flat()))
+// })
+//
+// module.exports = router
